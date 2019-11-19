c4e36aba4b1e436d742d6ee0c37e9df37eae371c
Nikunj Jain <nikunj.jain@myrealdata.net>
2019-11-19 17:03:40 +0530

    Change function names and remove uneeded variables

--- www/ProximityWake.js
+++ www/ProximityWake.js
@@ -1,20 +1,16 @@
 var exec = require('cordova/exec');
 
-exports.MODE_IN_COMMUNICATION = 100;
-exports.MODE_NORMAL = 101;
-exports.MODE_RINGTONE = 102;
-
-exports.requestFocus = function(mode, success, error) {
+exports.activateSensor = function(mode, success, error) {
     success = success || function() { };
     error = error || function() { };
 
-    exec(success, error, "ProximityWake", "requestFocus", [mode]);
+    exec(success, error, "ProximityWake", "activate", [mode]);
 };
 
 
-exports.dumpFocus = function(success, error) {
+exports.deactivateSensor = function(success, error) {
     success = success || function() { };
     error = error || function() { };
 
-    exec(success, error, "AudioFocus", "dumpFocus");
+    exec(success, error, "ProximityWake", "deactivate");
 };
